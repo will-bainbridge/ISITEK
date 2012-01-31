e37b58d31237bb97986c58c06028bad433ccb4da
Will Bainbridge <will.d.bainbridge@gmail.com>
2012-01-31 10:23:03 +0000

    added separate header for blas and lapack routines

---
+++ src/linear.h
@@ -0,0 +1,13 @@
+#ifndef LINEAR_H
+#define LINEAR_H
+
+// blas routines
+void dcopy_(int *n, double *X, int *incx, double *Y, int *incy);
+void dscal_(int *n, double *alpha, double *X, int *incx);
+void dgemv_(char *trans, int *m, int *n, double *alpha, double *A, int *lda, double *X, int *incx, double *beta, double *Y, int *incy);
+void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha, double *A, int *lda, double *B, int *ldb, double *beta, double *C, int *ldc);
+
+// lapack routines
+void dgesv_(int *n, int *nrhs, double *A, int *lda, int *ipiv, double *B, int *ldb, int *info);
+
+#endif
--- src/numerics.c
+++ src/numerics.c
@@ -1,17 +1,12 @@
 //////////////////////////////////////////////////////////////////
 
 #include "isitek.h"
+
 #include "constants.h"
+#include "linear.h"
 
 void basis(int n, double *phi, double **x, double *origin, double size, int index, int *differential);
 void transformation_matrix(int order, double **T, double **R);
-
-void dcopy_(int *n, double *X, int *incx, double *Y, int *incy);
-void dscal_(int *n, double *alpha, double *X, int *incx);
-void dgemv_(char *trans, int *m, int *n, double *alpha, double *A, int *lda, double *X, int *incx, double *beta, double *Y, int *incy); 
-void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha, double *A, int *lda, double *B, int *ldb, double *beta, double *C, int *ldc);
-
-void dgesv_(int *n, int *nrhs, double *A, int *lda, int *ipiv, double *B, int *ldb, int *info);
 
 //////////////////////////////////////////////////////////////////
 
